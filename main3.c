338b3d756377a2efc40a4f015aadcbb4e1c9fa69
alu0100851700 <alu0100851700@ull.edu.es>
2015-12-21 12:19:29 +0000

    main3 hecho, lectura desde ficheros implementada

--- main3.c
+++ main3.c
@@ -1,44 +1,70 @@
-#include <cstdio>
+#include "stdio.h"
 
-#define N 3
-#define M 5
+#define N 21    //COEFICIENTES
+#define M 3332  //MUESTRAS
 
 //INICIALIZACION COEFICIENTES
 float* coef_initialize(){
-    float* v_coef = (float *) malloc(N);
-    for(int i=0; i<N; i++)
-        v_coef[i] = 1/N;
+    float* v_coef = (float *) malloc(N * sizeof(float));
+
+    int i=0;
+    FILE *f_in = fopen ("Coeficientes.csv", "r");
+    while( fscanf (f_in, "%f", &v_coef[i]) != EOF && i<N) {
+        i++;
+    }
+    
+    fclose(f_in);
         
     return v_coef;
 }
 
 //INICIALIZACION VECTOR DE ENTRADA, PROLOGO y EPILOGO
 float* signal_read(){
-    float* v_in = (float *) malloc(M+N-1);
+    float* v_in = (float *) malloc((M+N-1) * sizeof(float));
+    float buffer;
+    
+    
+    
     //INICIALIZACION PROLOGO/EPILOGO
-    for(int i=0; i<(N-1)/2; i++){
+    int i;
+    for(i=0; i<(N-1)/2; i++){
         v_in[i]     = 0;
         v_in[M+i]   = 0;
     }
     
-    //INICIALIZACION VECTOR
-    for(int i=N-1; i<M; i++)
-        v_in[i] = ;
+    //INICIALIZACION VECTOR y LECTURA DATOS
+    i=(N-1)/2;
+    FILE *f_in = fopen ("IBEX35.csv", "r");
+    while( fscanf (f_in, "%f", &v_in[i]) != EOF) {
+        i++;
+    }
+    fclose(f_in);
         
     return v_in;
 }
 
 //APLICACION FILTRO FIR
-float* fir(){
-    float* v_out = (float *) malloc(M+N-1);
-    
-    for(int i=0; i<M+N-1; i++){
+float* fir(float* v_coef, float* v_in){
+    float* v_out = (float *) malloc((M+N-1) * sizeof(float));
+    int i;
+    int j;
+    for(i=0; i<M+N-1; i++){
         v_out[i] = 0;
-        for(int j=0; j<N ; j++)
-            v_out[i] += v_coef[i] * v_in[i-j]; 
+        for(j=0; j<N ; j++)
+            v_out[i] += v_coef[j] * v_in[i-j]; 
     }
     
     return v_out;
+}
+
+void guardarDatos(float* v_out){
+    FILE *f_out = fopen("salida.txt","w");
+    int i;
+    fprintf(f_out ,"Longitud del vector: %d y %d coeficientes\n", N, M);
+    for(i=0; i<M+N-1; i++)
+        fprintf(f_out, "%d\t%f\n", i+1, v_out[i]);
+    
+    fclose(f_out);
 }
 
 int main()
@@ -54,12 +80,12 @@ int main()
     v_in = signal_read();
     
     //Ejecución del FIR
-    v_out = fir();
+    v_out = fir(v_coef, v_in);
     
-    
+    guardarDatos(v_out); 
             
-    delete v_coef;
-    delete v_in;
-    delete v_out;
+    free(v_coef);
+    free(v_in);
+    free(v_out);
             
 }
